8babf90f44a36b8e5dd5c64c0f2ee525e196b463
tamerat-ab <tamerat.abera18@gmail.com>
2023-09-09 01:55:58 +0200

    final-2

--- network/models.py
+++ network/models.py
@@ -11,13 +11,7 @@ class Posts(models.Model):
     text_field=models.CharField(max_length=5000)
     likes=models.IntegerField(default=0)
     date_created_on=models.DateField(auto_now=True)
-    comment=models.TextField(blank=True)
-    is_liked=models.BooleanField(default=False)
-    count_likes=models.IntegerField(default=0)
     
-
-# class Follow(models.Model):
-#     user=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     
 class Follower(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
@@ -32,6 +26,10 @@ class Like(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     post=models.ForeignKey(Posts,on_delete=models.CASCADE)
     liking_user=models.IntegerField()
+class Comment(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    post=models.ForeignKey(Posts,on_delete=models.CASCADE)
+    comment=models.TextField(blank=True)
 
 
 
